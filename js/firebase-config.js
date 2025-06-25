43f164143600ac4011bb1e11fe2188223d29c407
TaamPham <47493768+bumbi12@users.noreply.github.com>
2025-06-25 10:17:03 +0000

    First commit with full Firebase setup

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRc5OmLgHgG57MGwkDvPQLHI74Gv58fgg",
+  authDomain: "minigotanuyen.firebaseapp.com",
+  projectId: "minigotanuyen",
+  storageBucket: "minigotanuyen.firebasestorage.app",
+  messagingSenderId: "656079740242",
+  appId: "1:656079740242:web:acf2bc7848c33cd1c7ae8b",
+  measurementId: "G-T5BSB98505"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
